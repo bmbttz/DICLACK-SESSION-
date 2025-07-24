8ff2de3762845ee72bb3924dda03c913ca319fe2
B.M.B-TECH <tzsailas@gmail.com>
2025-07-24 20:18:24 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "BMB-XMD~" + string_session;
+                        let md = "NOVA~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `╭━━━━━━━━━━━━━━━━━━━━━╮
 ┃  💖 B.M.B-XMD-V2 USER ✅  ┃
